4f8a20747f9eb28c2a4e0d9516f4a0bf958e800a
Mohammed <mohammedashour742@gmail.com>
2025-02-19 14:36:53 -0800

    Add circle functions

--- index.dart
+++ index.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 void main(){
     print("Hello, This is Mohammed");
     print("The area is ${getArea(5, 7)}");
     print("The circmfirance is ${getCircomfirance(5, 7)}");
     print("Back to the code...");
+    print("The area of the circle is ${getCircleArea(3.5)}");
+    print("The path of the circle is ${getCirclePath(3.5)}");
 }
 
 double getArea(double len, double wid){
@@ -11,4 +15,12 @@ double getArea(double len, double wid){
 
 double getCircomfirance(double len, double wid){
   return 2*(len + wid);
+}
+
+double getCircleArea(double radius){
+  return pi*radius*radius;
+}
+
+double getCirclePath(double radius){
+  return 2*pi*radius;
 }
