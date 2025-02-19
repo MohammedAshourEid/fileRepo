ada6d7d26c7af49d1946fdc4e2ff49f226debe81
Mohammed <mohammedashour742@gmail.com>
2025-02-19 13:09:39 -0800

    Calculating the path

--- index.dart
+++ index.dart
@@ -1,8 +1,13 @@
 void main(){
     print("Hello, This is Mohammed");
     print("The area is ${getArea(5, 7)}");
+    print("The circmfirance is ${getCircomfirance(5, 7)}");
 }
 
 double getArea(double len, double wid){
     return len * wid;
+}
+
+double getCircomfirance(double len, double wid){
+  return 2*(len + wid);
 }
